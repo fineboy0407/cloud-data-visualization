e11930fa29f5ea7f2652a4fe8299390fa302cfc6
fineboy0407 <levilee.fine@gmail.com>
2022-03-26 18:49:03 +0530

    Updated the app with Material Theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { darkTheme, lightTheme } from "./theme";
+import { Button, CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
+  const [light, setLight] = React.useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={light ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Button onClick={() => setLight(prev => !prev)}>Toggle Theme</Button>
+    </ThemeProvider>
   );
 }
 
